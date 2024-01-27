4dd62d04ed03577c9f74f48d230dc1dccd8585b6
uditha1994 <uditha@programmer.net>
2024-01-28 01:18:51 +0530

    first run backend

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
 import express from "express";
+import cors from "cors";
+import { sample_foodData } from "./data";
+
+const app = express();
+app.use(cors({
+    credentials:true,
+    origin:["http://localhost:4200"]
+}))
+
+app.get("/api/foods", (req,res)=>{
+    res.send(sample_foodData);
+})
+
+const port = 5000;
+app.listen(port, ()=>{
+    console.log("website served on http://localhost:"+port)
+})
