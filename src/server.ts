067f973f9c79e527991bd1d71df3ca2c1152b651
uditha1994 <uditha@programmer.net>
2024-01-28 01:28:45 +0530

    food serch 1

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,12 @@ app.get("/api/foods", (req,res)=>{
     res.send(sample_foodData);
 })
 
+app.get("/api/foods/search/:searchTerm", (req,res)=>{
+    const searchTerm = req.params.searchTerm;
+    const foods = sample_foodData.filter(food => food.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
+    res.send(foods);    
+})
+
 const port = 5000;
 app.listen(port, ()=>{
     console.log("website served on http://localhost:"+port)
