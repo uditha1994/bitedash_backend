cfa502b3603d0fea64d5a4ae71100a521eea79aa
uditha1994 <uditha@programmer.net>
2024-01-28 11:06:13 +0530

    backend tags, foodid

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { sample_foodData } from "./data";
+import { sample_foodData, sample_tags } from "./data";
 
 const app = express();
 app.use(cors({
@@ -16,6 +16,22 @@ app.get("/api/foods/search/:searchTerm", (req,res)=>{
     const searchTerm = req.params.searchTerm;
     const foods = sample_foodData.filter(food => food.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
     res.send(foods);    
+})
+
+app.get("/api/foods/tags", (req,res)=>{
+    res.send(sample_tags);
+})
+
+app.get("/api/foods/tag/:tagName", (req,res)=>{
+    const tagName = req.params.tagName;
+    const foods = sample_foodData.filter(Food => Food.tags?.includes(tagName));
+    res.send(foods);    
+})
+
+app.get("/api/foods/:foodId", (req,res)=>{
+    const foodId = req.params.foodId;
+    const food = sample_foodData.find(Food => Food.id == foodId);
+    res.send(food);
 })
 
 const port = 5000;
