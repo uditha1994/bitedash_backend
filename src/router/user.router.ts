be30218cab324bcb45e6b6eb71f7f94fe5d5aec1
uditha1994 <uditha@programmer.net>
2024-01-29 23:24:38 +0530

    login with db

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -19,17 +19,17 @@ router.get("/seed", asyncHandler(
     }
 ))
 
-router.post("/login", (req,res) => {
-    const {email, password} = req.body
-    const user = sample_users.find(user => user.email === email &&
-        user.password === password);
-
-        if(user){
-            res.send(generateTokenResponse(user));
-        }else{
-            res.status(400).send("Invalid Username or Password!!");
-        }
-})
+router.post("/login", asyncHandler(
+    async(req,res) => {
+        const {email, password} = req.body
+        const user = await UserModel.findOne({email,password});
+            if(user){
+                res.send(generateTokenResponse(user));
+            }else{
+                res.status(400).send("Invalid Username or Password!!");
+            }
+    }
+))
 
 const generateTokenResponse = (user:any)=>{
     const token = jwt.sign({
