06cef59e807dc4ca0e20b2510b261c575b68824d
uditha1994 <uditha@programmer.net>
2024-01-31 20:02:46 +0530

    login bug fix

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -22,16 +22,19 @@ router.get("/seed", asyncHandler(
 ))
 
 router.post("/login", asyncHandler(
-    async(req,res) => {
-        const {email, password} = req.body
-        const user = await UserModel.findOne({email});
-            if(user && (await bcrypt.compare(password,user.password))){
-                res.send(generateTokenResponse(user));
-            }else{
-                res.status(HTTP_BAD_REQUEST).send("Invalid Username or Password!!");
-            }
+    async (req, res) => {
+      const {email, password} = req.body;
+      const user = await UserModel.findOne({email});
+    
+       if(user && (await bcrypt.compare(password,user.password))) {
+        res.send(generateTokenReponse(user));
+       }
+       else{
+         res.status(HTTP_BAD_REQUEST).send("Username or password is invalid!");
+       }
+    
     }
-))
+  ))
 
 router.post('/register', asyncHandler(
     async (req,res) => {
@@ -52,25 +55,25 @@ router.post('/register', asyncHandler(
             isAdmin: false
         }
         const dbUser = await UserModel.create(newUser);
-        res.send(generateTokenResponse(dbUser));
+        res.send(generateTokenReponse(dbUser));
     }
 ))
 
-const generateTokenResponse = (user:any)=>{
+const generateTokenReponse = (user : User) => {
     const token = jwt.sign({
-        id:user.id, email:user.email, isAdmin:user.isAdmin
-    }, process.env.JWT_SECRET!, {
-        expiresIn:"30d"
+      id: user.id, email:user.email, isAdmin: user.isAdmin
+    },process.env.JWT_SECRET!,{
+      expiresIn:"30d"
     });
-
+  
     return {
-        id:user.id,
-        email:user.email,
-        name:user.name,
-        address:user.address,
-        isAdmin:user.isAdmin,
-        token: token
-    }
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      address: user.address,
+      isAdmin: user.isAdmin,
+      token: token
+    };
 }
 
 export default router;
